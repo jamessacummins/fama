e6686cff8cc9567b6921c27b58762c092fb3f6e6
jamessacummins <jamessacummins@gmail.com>
2020-08-28 20:28:08 +1000

    Updates

---
+++ precache-manifest.9d60a2beb408282ddecf64f4c3d5de9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71da2082b8394330af086d6dff143c48",
+    "url": "/fama/index.html"
+  },
+  {
+    "revision": "b0e7eb6e71bd4e1fa57c",
+    "url": "/fama/static/css/main.707744de.chunk.css"
+  },
+  {
+    "revision": "defbb1fa6bf1d13b6896",
+    "url": "/fama/static/js/2.7a17ac28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fama/static/js/2.7a17ac28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e7eb6e71bd4e1fa57c",
+    "url": "/fama/static/js/main.296fc572.chunk.js"
+  },
+  {
+    "revision": "805df8a3b197e257b5b4",
+    "url": "/fama/static/js/runtime-main.17358e3b.js"
+  },
+  {
+    "revision": "44c4421e97b8e45ccb544a5b2bbb491a",
+    "url": "/fama/static/media/gear.44c4421e.png"
+  },
+  {
+    "revision": "1d3e26f7f4cc316fcf718a74b10be75a",
+    "url": "/fama/static/media/logo.1d3e26f7.png"
+  },
+  {
+    "revision": "d3763524d46565fc2d9c1e4ff4abfdf7",
+    "url": "/fama/static/media/search.d3763524.png"
+  }
+]);
